3d72ccc6333f0305e99a5463c7bb61bd2134b04c
shehjar <kaul.shehjar@gmail.com>
2017-05-19 10:07:10 +0200

    Code completed till the prediction step

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -13,11 +13,25 @@
 #include "particle_filter.h"
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
-	// TODO: Set the number of particles. Initialize all particles to first position (based on estimates of 
-	//   x, y, theta and their uncertainties from GPS) and all weights to 1. 
+	// TODO: Set the number of particles. Initialize all particles to first position (based on estimates of
+	//   x, y, theta and their uncertainties from GPS) and all weights to 1.
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
-
+	num_particles = 1000;
+	std::default_random_engine gen_init;
+	std::normal_distribution<double> dist_x(x, std[0]);
+	std::normal_distribution<double> dist_y(y, std[1]);
+	std::normal_distribution<double> dist_theta(theta, std[2]);
+	for (int i=0; i<num_particles; i++){
+	  Particle sample_particle;
+    sample_particle.id = i;
+    sample_particle.x = dist_x(gen_init);
+    sample_particle.y = dist_y(gen_init);
+    sample_particle.theta = dist_theta(gen_init);
+    sample_particle.weight = 1.0;
+    particles.push_back(sample_particle);
+	}
+  is_initialized = true;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], double velocity, double yaw_rate) {
@@ -25,18 +39,39 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 	// NOTE: When adding noise you may find std::normal_distribution and std::default_random_engine useful.
 	//  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
 	//  http://www.cplusplus.com/reference/random/default_random_engine/
-
+  std::default_random_engine gen_pred;
+  std::normal_distribution<double> dist_x(0,std_pos[0]);
+  std::normal_distribution<double> dist_y(0,std_pos[1]);
+  std::normal_distribution<double> dist_theta(0,std_pos[2]);
+  for (int i=0; i<num_particles; i++){
+    //Updating theta
+    double init_theta = particles[i].theta;
+    particles[i].theta += yaw_rate*delta_t;
+    // updating x and y on the basis of yaw rate
+    if (yaw_rate > 1e-3){
+      particles[i].x += (velocity/yaw_rate)*(sin(particles[i].theta) - sin(init_theta));
+      particles[i].y += (velocity/yaw_rate)*(-cos(particles[i].theta) + cos(init_theta));
+    }
+    else{
+      particles[i].x += velocity*cos(init_theta)*delta_t;
+      particles[i].y += velocity*sin(init_theta)*delta_t;
+    }
+    // Adding Noise
+    particles[i].x += dist_x(gen_pred);
+    particles[i].y += dist_y(gen_pred);
+    particles[i].theta += dist_theta(gen_pred);
+  }
 }
 
 void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs>& observations) {
-	// TODO: Find the predicted measurement that is closest to each observed measurement and assign the 
+	// TODO: Find the predicted measurement that is closest to each observed measurement and assign the
 	//   observed measurement to this particular landmark.
-	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to 
+	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to
 	//   implement this method and use it as a helper during the updateWeights phase.
 
 }
 
-void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
+void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 		std::vector<LandmarkObs> observations, Map map_landmarks) {
 	// TODO: Update the weights of each particle using a mult-variate Gaussian distribution. You can read
 	//   more about this distribution here: https://en.wikipedia.org/wiki/Multivariate_normal_distribution
@@ -45,14 +80,14 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   Keep in mind that this transformation requires both rotation AND translation (but no scaling).
 	//   The following is a good resource for the theory:
 	//   https://www.willamette.edu/~gorr/classes/GeneralGraphics/Transforms/transforms2d.htm
-	//   and the following is a good resource for the actual equation to implement (look at equation 
-	//   3.33. Note that you'll need to switch the minus sign in that equation to a plus to account 
+	//   and the following is a good resource for the actual equation to implement (look at equation
+	//   3.33. Note that you'll need to switch the minus sign in that equation to a plus to account
 	//   for the fact that the map's y-axis actually points downwards.)
 	//   http://planning.cs.uiuc.edu/node99.html
 }
 
 void ParticleFilter::resample() {
-	// TODO: Resample particles with replacement with probability proportional to their weight. 
+	// TODO: Resample particles with replacement with probability proportional to their weight.
 	// NOTE: You may find std::discrete_distribution helpful here.
 	//   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
 
